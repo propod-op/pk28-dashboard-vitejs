1e093622c609fb16151283a820bf75b60162048c
POCHIC Olivier <o.pochic@gmail.com>
2024-03-18 19:04:34 +0100

    Last dashboard

--- src/components/ContentEdit.jsx
+++ src/components/ContentEdit.jsx
@@ -8,6 +8,7 @@ const ContentEdit = () => {
 
 	return (
 		<>
+			<div className="title">Promotion du moment</div>
 			<Box sx={{ width: "100%" }}>
 				<Grid container spacing={0} sx={{ width: 1 }}>
 					<TextField sx={{ width: "100%" }} id="promotion-text" name="promotion-text" placeholder="Description du produit de la semaine" variant="outlined" helperText="Produit en promotion" value={""} onChange={handleChangeProductOfTheWeek} />
@@ -15,7 +16,23 @@ const ContentEdit = () => {
 				<Grid item sx={1}>
 					<div className="filezone">
 						<label className="label-filezone" htmlFor="imageProduitPromotion">
-							Ajouter une image
+							Image du produit en promotion du moment
+						</label>
+						<TextField sx={{ mr: 2 }} type="file" name="imageKoi" id="imageKoi" onChange={handleImageChange} inputRef={fileInputRef} required />
+					</div>
+				</Grid>
+			</Box>
+			<div className="title" style={{ marginTop: "2rem" }}>
+				Carpe du moment
+			</div>
+			<Box sx={{ width: "100%" }}>
+				<Grid container spacing={0} sx={{ width: 1 }}>
+					<TextField sx={{ width: "100%" }} id="promotion-text" name="promotion-text" placeholder="Description de la carpe du moment" variant="outlined" helperText="Produit en promotion" value={""} onChange={handleChangeProductOfTheWeek} />
+				</Grid>
+				<Grid item sx={1}>
+					<div className="filezone">
+						<label className="label-filezone" htmlFor="imageProduitPromotion">
+							Image de la carpe du moment
 						</label>
 						<TextField sx={{ mr: 2 }} type="file" name="imageKoi" id="imageKoi" onChange={handleImageChange} inputRef={fileInputRef} required />
 					</div>
--- src/pages/ContentPage.jsx
+++ src/pages/ContentPage.jsx
@@ -4,9 +4,6 @@ import "../styles/interface.css";
 const ContentPage = () => {
 	return (
 		<div className="page-content">
-			<div className="title" style={{}}>
-				Editeur de contenus
-			</div>
 			<ContentEdit />
 		</div>
 	);
